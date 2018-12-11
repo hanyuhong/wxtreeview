60092dbcb9d23f0f6ce67d204e2c0b589589fb88
hanyuhong <7327741@qq.com>
2018-12-11 23:21:21 +0800

    Add files via upload

---
+++ pages/treeview/index.js
@@ -0,0 +1,28 @@
+const app = getApp()
+
+Page({
+	
+	 data:{
+		 expendLevel:1,//展开的层级，从0开始
+	 },
+	onLoad(option){
+		var that = this
+		wx.request({
+			url:app.globalData.url2 + '/org/getAllDptTree/true/true/4',
+			data:{
+				id: 0//app.globalData.cabiUserInfo.dptId
+			},
+			success(res) {
+				that.treeValue = res.data
+				that.setData({
+					
+					treeValue : res.data
+				})
+			}
+		})
+		 
+	},
+	
+	
+
+})
